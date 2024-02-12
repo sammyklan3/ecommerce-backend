2e67799404be3e188e4adf8c067c848c7cb4c24d
sammyklan3 <137900210+sammyklan3@users.noreply.github.com>
2024-02-12 19:39:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ const upload = multer({
 
 
 const db = mysql.createPool({
-    host: "http://200.134.155.78",
+    host: "200.134.155.78",
     user: "root",
     password: "",
     database: "phonepartsstore",
